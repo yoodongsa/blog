777ae1792bf257225d3ad72f3ffdd2d0bddaef09
사유동 <sayd8472@gmail.com>
2024-03-29 17:31:13 +0900

    부모 자식 상태 변경 및 속성 상속 개념 추가

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import { useState } from 'react';
 
 function App() {
 
-  let [titles, titleState] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [likes, likeState] = useState(0);
+  let [titles, setTitles] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [title, setTitle] = useState(0);
+  let [likes, setLike] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
 
-  function like() {
-    likeState(likes + 1);
+  function like(i) {
+    let copy = [...likes];
+    copy[i] = likes[i]+1;
+    setLike(copy);
   }
 
   function changeTitle1() {
     let copy = [...titles];
     copy[0] = '여자 코트 추천';
-    titleState(copy);
+    setTitles(copy);
   }
-
+  
   function sortByChosung() {
     let sortTitles = [...titles.sort()];
-    titleState(sortTitles);
+    setTitles(sortTitles);
   }
-
+  
   return (
     <div className="App">
       <div className='black-nav'>
@@ -30,27 +34,32 @@ function App() {
       {
         titles.map((title, i) => (
           <div className='list' key={i}>
-            <h4>{title}<span onClick={like}>👍</span> {likes}</h4>
+            <h4 onClick={()=>{setTitle(i); setModal(!modal);}}>{title}</h4><span onClick={()=> like(i)}>👍</span> {likes[i]}
             <p>2/17 발행</p>
           </div>
         ))
       }
 
-      <Modal></Modal>
+      <input type="text" />
+
+      {
+        modal ? <Modal titles={titles} title={title} changeTitle1={changeTitle1}/> : null
+      }
 
       <button onClick={changeTitle1}>제목 변경하기</button>
       <button onClick={sortByChosung}>제목 정렬하기</button>
     </div>
   );
-
+  
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className='modal'>
-      <h4>제목</h4>
+      <h4>{props.titles[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.changeTitle1}>글수정</button>
     </div>
   )
 }
